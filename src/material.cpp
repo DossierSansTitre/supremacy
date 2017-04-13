d9d55fa8668d48dd6acd658f8bf0acce6fc1bb86
Nax <max@bacoux.com>
2017-04-13 13:22:55 +0200

    Made empty space dark gray instead of black

--- src/material.cpp
+++ src/material.cpp
@@ -1,7 +1,7 @@
 #include <material.h>
 
 const Material Material::materials[] = {
-    {0, 0, 0, 0},
+    {0, 233, 0, 233},
     {202, 208, 130, 136},
     {255, 250, 247, 244},
     {178, 172, 136, 130}
