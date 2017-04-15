70269650e8f051a8d53d9c66254914e5aa8606d4
Nax <max@bacoux.com>
2017-04-15 19:30:38 +0200

    Inline map

--- src/map.h
+++ src/map.h
@@ -13,22 +13,73 @@ public:
     Map();
     ~Map();
 
+    int width() const
+    {
+        return _width;
+    }
+
+    int height() const
+    {
+        return _height;
+    }
+
+    int depth() const
+    {
+        return _depth;
+    }
+
+    void at(int x, int y, int z, TileID& out_tile, MaterialID& out_material) const
+    {
+        int i = index(x, y, z);
+        out_tile = tile_at(i);
+        out_material = material_at(i);
+    }
+
+    TileID tile_at(int i) const
+    {
+        if (i == -1)
+            return TileID::None;
+        return _tiles[i];
+    }
+
+    TileID tile_at(int x, int y, int z) const
+    {
+        return tile_at(index(x, y, z));
+    }
+
+    MaterialID material_at(int i) const
+    {
+        if (i == -1)
+            return MaterialID::None;
+        return _materials[i];
+    }
+
+    MaterialID material_at(int x, int y, int z) const
+    {
+        return material_at(index(x, y, z));
+    }
+
+    bool visible(int i) const
+    {
+        if (i == -1)
+            return false;
+        return _visible[i];
+    }
+
+    bool visible(int x, int y, int z) const
+    {
+        return visible(index(x, y, z));
+    }
+
+    int index(int x, int y, int z) const
+    {
+        if (x < 0 || x >= _width || y < 0 || y >= _height || z < 0 || z >= _depth)
+            return -1;
+        return x + y * _width + z * _width * _height;
+    }
+
     void        create(int width, int height, int depth);
     void        destroy();
-
-    int         width() const { return _width; }
-    int         height() const { return _height; }
-    int         depth() const { return _depth; }
-
-    void        at(int x, int y, int z, TileID& out_tile, MaterialID& out_material) const;
-    TileID      tile_at(int index) const;
-    TileID      tile_at(int x, int y, int z) const;
-    MaterialID  material_at(int index) const;
-    MaterialID  material_at(int x, int y, int z) const;
-    bool        visible(int index) const;
-    bool        visible(int x, int y, int z) const;
-
-    int         index(int x, int y, int z) const;
 
     void        set_tile(int index, TileID tile);
     void        set_tile(int x, int y, int z, TileID tile);
