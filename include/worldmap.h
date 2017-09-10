52aa944e620efc25bfaa019237aac907a6cd300e
Nax <max@bacoux.com>
2017-09-10 14:14:33 +0200

    Add getters to worldmap

--- include/worldmap.h
+++ include/worldmap.h
@@ -16,8 +16,13 @@ public:
     Worldmap(u16 id, Vector2i size);
 
     u16         id() const { return _id; }
+    size_t      index(Vector2i pos) const { return pos.x + _size.x * pos.y; }
     Vector2i    size() const { return _size; }
-    BiomeID     biome(Vector2i pos) const { return _biomes[pos.x + pos.y * _size.x]; }
+    BiomeID     biome(Vector2i pos) const { return _biomes[index(pos)]; }
+    u16         height(Vector2i pos) const { return _height[index(pos)]; }
+    u16         temperature(Vector2i pos) const { return _temperature[index(pos)]; }
+    u16         rain(Vector2i pos) const { return _rain[index(pos)]; }
+    u16         drainage(Vector2i pos) const { return _drainage[index(pos)]; }
 
 private:
     u16             _id;
