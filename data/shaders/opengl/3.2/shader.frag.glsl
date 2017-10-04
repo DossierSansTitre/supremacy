0bcee28a048aee13ed6b88f9e003546799442c40
Nax <max@bacoux.com>
2017-10-04 14:02:48 +0200

    Small optimizations

--- data/shaders/opengl/3.2/shader.frag.glsl
+++ data/shaders/opengl/3.2/shader.frag.glsl
@@ -14,13 +14,12 @@ out vec3 color;
 void main()
 {
     float sym = texture(uSymbol, fTexCoord).r;
-    vec2 sym_coord = vec2(mod(sym, 32.0), floor(sym / 32.0));
-    vec2 tcoord = (fract(uTileCount * fTexCoord) + sym_coord) / 32.0;
+    vec2 sym_coord = vec2(mod(sym, 32.0), floor(sym * 0.03125));
+    vec2 tcoord = (fract(uTileCount * fTexCoord) + sym_coord) * 0.03125;
 
-    vec4 ccolor = vec4(texture(uColor, fTexCoord).rgb, 1.0);
-    vec4 csymbol = texture(uTexture, tcoord);
-    vec3 fcolor = vec3(ccolor * csymbol);
+    vec3 ccolor = texture(uColor, fTexCoord).rgb;
+    vec4 tex = texture(uTexture, tcoord);
+    vec3 fcolor = vec3(ccolor * tex.rgb);
     vec3 bcolor = texture(uColorBg, fTexCoord).rgb;
-    float a = csymbol.a;
-    color = mix(bcolor, fcolor, a);
+    color = mix(bcolor, fcolor, tex.a);
 }
